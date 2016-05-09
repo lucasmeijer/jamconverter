d33d7f2f3ca10a12f44ef02ba94fa22ca346f1d6
Lucas Meijer <lucas@unity3d.com>
2016-05-09 18:47:01 +0200

    add playground test

---
+++ Tests/Playground.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NiceIO;
+using NUnit.Framework;
+
+namespace jamconverter.Tests
+{
+	[TestFixture]
+	class Playground
+	{
+		[Test]
+		public void A()
+		{
+			var converter = new JamToCSharpConverter();
+			var output = converter.Convert(new NPath("c:/unity/Projects/Jam/RuntimeFiles.jam").ReadAllText());
+			var file = NiceIO.NPath.SystemTemp.Combine("PlayGround.cs");
+
+			new CSharpRunner().Run(output, new[] { new NPath("c:/jamconverter/bin/runtimelib.dll") }).Select(s => s.TrimEnd());
+
+
+			file.WriteAllText(output);
+			Console.WriteLine(file);
+		}
+	}
+}
