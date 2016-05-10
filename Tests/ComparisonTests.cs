a9a614c1ea2dee23616ec96b9732b82b48ae1228
Jonas Echterhoff <jonas@unity3d.com>
2016-05-10 18:09:02 +0100

    Fix some hardcode paths

--- Tests/ComparisonTests.cs
+++ Tests/ComparisonTests.cs
@@ -594,7 +594,7 @@ on $(containsmytarget) { Echo $(myvar) ;  Echo $(myvar2) ; }
             try
             {
                 var csharp = new JamToCSharpConverter().Convert(simpleProgram);
-                csharpResult = new CSharpRunner().Run(csharp, new[] {new NPath("c:/jamconverter/bin/runtimelib.dll")}).Select(s => s.TrimEnd());
+				csharpResult = new CSharpRunner().Run(csharp, new[] {JamRunner.ConverterRoot.Combine(new NPath("bin/runtimelib.dll"))}).Select(s => s.TrimEnd());
 
                 Console.WriteLine("C#:");
                 foreach (var l in csharpResult)
