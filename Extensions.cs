8f59980c809869ba802cb40a4e0020dae3d6c5db
Jonas Echterhoff <jonas@unity3d.com>
2016-05-10 21:41:07 +0100

    DistinctBy

--- Extensions.cs
+++ Extensions.cs
@@ -15,6 +15,19 @@ namespace jamconverter
                 yield return e;
         }
 
+		public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+		(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+		{
+			HashSet<TKey> seenKeys = new HashSet<TKey>();
+			foreach (TSource element in source)
+			{
+				if (seenKeys.Add(keySelector(element)))
+				{
+					yield return element;
+				}
+			}
+		}
+
         public static string SeperateWithSpace(this IEnumerable<String> values )
         {
             return SeperateWith(values, " ");
