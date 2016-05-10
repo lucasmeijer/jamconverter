2b1cf518520ca9f9c76106e308348b283c7ee772
Jonas Echterhoff <jonas@unity3d.com>
2016-05-10 23:05:37 +0100

    Revert Rene's changes for now to get quote tests to work again

--- Scanner.cs
+++ Scanner.cs
@@ -90,10 +90,10 @@ namespace jamconverter
                 if (!char.IsWhiteSpace(_input[nextChar]))
                     _insideVariableExpansionModifierSpan = true;
 
-		    return new ScanToken() {tokenType = TokenTypeFor(literal), literal = literal};
-	    }
-		
-	    private TokenType TokenTypeFor(string literal)
+            return new ScanToken() {tokenType = TokenTypeFor(literal), literal = literal.Replace ("\\", "")};
+        }
+        
+        private TokenType TokenTypeFor(string literal)
         {
             switch (literal)
             {
@@ -166,43 +166,41 @@ namespace jamconverter
                     return TokenType.Literal;
             }
         }
-
-		private StringBuilder _builder = new StringBuilder();
         
         private string ReadLiteral(bool allowColon)
         {
             int i;
-	        bool inQuote = false;
-            for (i = nextChar; i < _input.Length; i++)
+            bool isQuoteLiteral = _input[nextChar] == '"';
+
+            for (i = nextChar; i != _input.Length; i++)
             {
-				//dont allow colons as the first character
-	            bool reallyAllowCon = allowColon && nextChar != i;
-				char ch = _input[i];
-	            if (IsLiteral(ch, reallyAllowCon) || (ch == '$' && (i + 1) < _input.Length && _input[i + 1] != '(')) // Prevent single $ inside literal being treated as DereferenceVariable token.
-	            {
-		            if (ch == '\\' && (i + 1) < _input.Length)
-		            {
-			            ++i;
-			            _builder.Append(_input[i]);
-		            }
-		            else
-			            _builder.Append(ch);
-		            continue;
-	            }
-	            break;
+                //dont allow colons as the first character
+                bool reallyAllowCon = allowColon && nextChar != i;
+                if (isQuoteLiteral) 
+                {
+                    if (i == nextChar || _input [i] != '"' || _input [i-1] == '\\')
+                        continue;
+                    i++;
+                } 
+                else 
+                {
+                    if (IsLiteral (_input [i], reallyAllowCon))
+                        continue;
+                }
+                break;
             }
-			
-			// Return special characters recognized by TokenForLiteral from here
-			// even though that doesn't make any sense.
+
             if (i == nextChar)
             {
                 nextChar++;
                 return _input[i].ToString();
             }
 
-	        var result = _builder.ToString();
-	        _builder.Clear();
-
+            string result;
+            if (isQuoteLiteral)
+                result = _input.Substring(nextChar+1, i - nextChar -2);
+            else
+                result = _input.Substring(nextChar, Math.Max(1,i - nextChar));
             nextChar = i;
             return result;
         }
